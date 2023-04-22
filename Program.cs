630db092f6a7f1af947cc6f4e46f8d245a3f2c1f
sidarava <val.sidarava@gmail.com>
2023-04-22 14:48:59 +0200

    lera cool

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection.Metadata;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Print calculation output of square area and perimeter
+        Square mySquare = new Square(5);
+        double squarePerimeter = mySquare.Perimeter();
+        double squareArea = mySquare.Area();
+        Console.WriteLine("Square perimeter is {0}, square area is {1}.", squarePerimeter, squareArea);
+
+        //Print calculation output of triangle area and perimeter
+        Triangle myTriangle = new Triangle(5, 5, 10);
+        double TrianglePerimeter = myTriangle.Perimeter();
+        double TriangleArea = myTriangle.Area();
+        Console.WriteLine("Triangle perimeter is {0}, triangle area is {1}.", TrianglePerimeter, TriangleArea);
+
+
+        //Print calculation output of circle area and perimeter
+        Circle myCircle = new Circle(10);
+        double CirclePerimeter = myCircle.Perimeter();
+        double CircleArea = myCircle.Area();
+        Console.WriteLine("Circle perimeter is {0}, circle area is {1}.", CirclePerimeter, CircleArea);
+
+
+
+    }
+}
---
+++ shapes.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+public abstract class Shape
+{
+    public abstract double Area();
+    public abstract double Perimeter();
+}
+
+public class Square : Shape
+{
+    private double side;
+
+    public Square(double side)
+    {
+        this.side = side;
+    }
+
+    public override double Area()
+    {
+        return side * side;
+    }
+
+    public override double Perimeter()
+    {
+        return 4 * side;
+    }
+}
+
+public class Triangle : Shape
+{
+    private double side1, side2, side3;
+
+    public Triangle(double side1, double side2, double side3)
+    {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public override double Area()
+    {
+        double p = (side1 + side2 + side3) / 2;
+        return Math.Sqrt(p * (p - side1) * (p - side2) * (p - side3));
+    }
+
+    public override double Perimeter()
+    {
+        return side1 + side2 + side3;
+    }
+}
+
+public class Circle : Shape
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public override double Area()
+    {
+        return Math.PI * radius * radius;
+    }
+
+    public override double Perimeter()
+    {
+        return 2 * Math.PI * radius;
+    }
+}
+
+
